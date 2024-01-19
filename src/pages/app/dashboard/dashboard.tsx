fb2dd86a8f083be0ce41c4ce5581d3f1ff38646a
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-01-19 15:54:23 -0300

    adicionando cards página dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,23 +1,20 @@
-import { DollarSign } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
+import { DayOrdersAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { MonthOrdersAmountCard } from './month-orders-amount-card'
+import { MonthRevenueCard } from './month-revenue-card'
 
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
       <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>/
         <div className="grid grid-cols-4 gap-4">
-          <Card>
-            <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-base font-semibold">
-                Receita total (mês)
-              </CardTitle>
-              <DollarSign className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-          </Card>
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
         </div>
       </div>
     </>
