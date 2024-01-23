a3b4fb49b539bab93562ee099e968f50dc9c91fd
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-01-22 22:35:23 -0300

    autenticação do usuário com o email

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -7,6 +7,8 @@ import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { useMutation } from '@tanstack/react-query'
+import { signIn } from '@/api/sign-in'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -21,9 +23,14 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await authenticate({email: data.email})
+
 
       toast.success('Enviamos um link de autenticação para o seu e-mail', {
         action: {
