b36d99a27ef39e5090f921402b73a793c4798b36
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-03 02:19:21 -0300

    perfil no cabeçalho

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,9 @@
 import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { getMenagedRestaurant } from '@/api/get-managed-restaurant'
+import { getProfile } from '@/api/get-profile'
 
 import { Button } from './ui/button'
 import {
@@ -11,6 +15,16 @@ import {
 } from './ui/dropdown-menu'
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getMenagedRestaurant,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,16 +32,16 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          Pizza Shop
+          {managedRestaurant?.name}
           <ChevronDown />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Pedro Moreira</span>
+          <span>{profile?.name}</span>
           <span className="text-sm font-normal text-muted-foreground">
-            pedro@email.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,8 @@
-import { env } from '@/env';
-import axios from 'axios';
+import axios from 'axios'
+
+import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 })
