9417f0c62e322ba7b8f58c379461cfbee315cd56
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-04 11:02:44 -0300

    modal de atualização de perfil

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -14,6 +14,9 @@ import {
   DropdownMenuSeparator,
 } from './ui/dropdown-menu'
 import { Skeleton } from './ui/skeleton'
+import { Dialog } from '@radix-ui/react-dialog'
+import { DialogTrigger } from './ui/dialog'
+import { StoreProfileDialog } from './store-profile-dialog'
 
 export function AccountMenu() {
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
@@ -27,46 +30,52 @@ export function AccountMenu() {
   })
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="flex select-none items-center gap-2"
-        >
-          {isLoadingManagedRestaurant ? (
-            <Skeleton className='h-4 w-40' />
-          ) : (managedRestaurant?.name)}
-          <ChevronDown />
-        </Button>
-      </DropdownMenuTrigger>
+    <Dialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="flex select-none items-center gap-2"
+          >
+            {isLoadingManagedRestaurant ? (
+              <Skeleton className='h-4 w-40' />
+            ) : (managedRestaurant?.name)}
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          {isLoadingProfile ? (
-            <div className='space-y-1.5'>
-              <Skeleton className='h-4 w-32'/>
-              <Skeleton className='h-3 w-24'/>
-            </div>
-          ) : (
-            <>
-              <span>{profile?.name}</span>
-              <span className="text-sm font-normal text-muted-foreground">
-                {profile?.email}
-              </span>
-            </>
-          )}
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <Building className="mr-2 h-4 w-4" />
-          <span>Perfil da loja</span>
-        </DropdownMenuItem>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel className="flex flex-col">
+            {isLoadingProfile ? (
+              <div className='space-y-1.5'>
+                <Skeleton className='h-4 w-32' />
+                <Skeleton className='h-3 w-24' />
+              </div>
+            ) : (
+              <>
+                <span>{profile?.name}</span>
+                <span className="text-sm font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+            )}
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DialogTrigger>
+            <DropdownMenuItem>
+              <Building className="mr-2 h-4 w-4" />
+              <span>Perfil da loja</span>
+            </DropdownMenuItem>
+          </DialogTrigger>
 
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="mr-2 h-4 w-4" />
-          <span>Sair</span>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Sair</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <StoreProfileDialog />
+    </Dialog>
   )
 }
---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,63 @@
+import { useQuery } from "@tanstack/react-query"
+import { Button } from "./ui/button"
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+import { Textarea } from "./ui/textarea"
+import { getMenagedRestaurant } from "@/api/get-managed-restaurant"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver} from "@hookform/resolvers/zod"
+
+const storeProfileSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+})
+
+type StoreProfileSchema = z.infer<typeof storeProfileSchema>
+
+export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getMenagedRestaurant,
+  })
+
+  const {
+    register,
+    handleSubmit
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? '',
+    }
+  })
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>Atualize as informações do seu estabelecimento visiveis ao seu cliente</DialogDescription>
+      </DialogHeader>
+
+      <form action="">
+        <div className="space-y-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">Nome</Label>
+            <Input className='col-span-3' id='name' {...register('name')} />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">Descrição</Label>
+            <Textarea className='col-span-3' id='description' {...register('description')}/>
+          </div>
+        </div>
+
+        <DialogFooter>
+          <Button type='button' variant='ghost'>Cancelar</Button>
+          <Button type="submit" variant='success'>Salvar</Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  )
+}
