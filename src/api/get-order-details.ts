88a6b99e83cc4f1f02f72fe7cbef72b177f41d7f
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-04 20:06:44 -0300

    detalhes do pedido

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,31 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrderDetailsProps {
+  orderId: string
+}
+
+export interface GetOrderDetailsResponse {
+  id: string
+  createdAt: string
+  status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+  totalInCents: number
+  customer: {
+    name: string
+    email: string
+    phone: string | null
+  }
+  orderItems: {
+    id: string
+    priceInCents: number
+    quantity: number
+    product: {
+      name: string
+    }
+  }[]
+}
+
+export async function GetOrderDetails({ orderId }: GetOrderDetailsProps) {
+  const response = await api.get<GetOrderDetailsResponse>(`/orders/${orderId}`)
+
+  return response.data
+}
